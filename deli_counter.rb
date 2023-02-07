d02e4cbe662f2b51e28c7a70218c1e15fbd59ab4
Osorabrian <osorabrian@gmail.com>
2023-02-07 22:04:35 +0300

    completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line arr
+    if arr.length == 0
+       puts "The line is currently empty."
+    else
+        puts "The line is currently: " + arr.map.with_index{|b,index| "#{index+1}. #{b}"}.join(" ")
+    end
+end
+
+def take_a_number arr, str
+    if arr.length == 0
+        arr.push(str)
+        puts "Welcome, #{str}. You are number 1 in line."
+    elsif arr.length > 0
+        arr << str
+        puts "Welcome, #{str}. You are number #{arr.length} in line."
+    end
+end
+
+def now_serving arr
+    if arr.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{arr.shift()}."
+    end
+end
